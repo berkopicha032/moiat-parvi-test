342db71320a9ce488bd375ace280d985f461192a
berkopicha032 <berkb3051@gmail.com>
2026-02-05 10:07:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+import streamlit as st
 
+
+st.title("Моето първо приложение")
+
+
+name = st.text_input("Как се казваш?")
+
+
+if name:
+    st.success(f"Здравей, {name}!") 
+    
+    st.write("---") 
+    
+   
+    st.subheader("Малък тест за теб:")
+    answer = st.number_input("Колко е 5 × 5?", step=1, value=0)
+    
+ 
+    if st.button("Провери"):
+        if answer == 25:
+            st.success("Точно така! Браво! 🎉")
+            st.balloons()
+        else:
+            st.error("Хмм, не е това. Опитай пак! 🧐")
