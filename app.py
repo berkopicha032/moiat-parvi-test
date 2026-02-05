6dabbdb7b3bdf62cfb31b3fc38fbe8fe0c35b61f
berkopicha032 <berkb3051@gmail.com>
2026-02-05 10:09:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 import streamlit as st
 
-
 st.title("Моето първо приложение")
-
 
 name = st.text_input("Как се казваш?")
 
-
 if name:
-    st.success(f"Здравей, {name}!") 
+     st.success(f"Здравей, {name}! 👋")
     
-    st.write("---") 
-    
-   
-    st.subheader("Малък тест за теб:")
+    st.write("---")
+
     answer = st.number_input("Колко е 5 × 5?", step=1, value=0)
     
- 
     if st.button("Провери"):
         if answer == 25:
-            st.success("Точно така! Браво! 🎉")
+            st.success("Вярно! 🎉")
             st.balloons()
         else:
-            st.error("Хмм, не е това. Опитай пак! 🧐")
+            st.error("Грешно. Опитай пак!")
